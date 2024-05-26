43994a52f683d951feffa7d9bdb0f3ca9e20de53
xvenusd <zxdmateozxd@gmail.com>
2024-05-25 21:46:47 -0500

    docker file

--- msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -18,8 +18,8 @@ public class UsuarioController {
     private UsuarioService service;
 
     @GetMapping
-    public List<Usuario> listar() {
-        return service.listar();
+    public Map<String, List<Usuario>> listar() {
+        return Collections.singletonMap("usuarios", service.listar());
     }
 
     @GetMapping("/{id}")
@@ -41,7 +41,7 @@ public class UsuarioController {
         if (!usuario.getEmail().isEmpty() && service.existePorEmail(usuario.getEmail())) {
             return ResponseEntity.badRequest()
                     .body(Collections
-                            .singletonMap("mensaje", "Ya existe un usuario con ese correo electronico!"));
+                            .singletonMap("mensaje", "Ya existe! un usuario con ese correo electronico!"));
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
     }
@@ -61,7 +61,7 @@ public class UsuarioController {
                     service.porEmail(usuario.getEmail()).isPresent()) {
                 return ResponseEntity.badRequest()
                         .body(Collections
-                                .singletonMap("mensaje", "Ya existe un usuario con ese correo electronico!"));
+                                .singletonMap("mensaje", "Ya exist un users con ese email electrónico !"));
             }
 
             usuarioDb.setNombre(usuario.getNombre());
